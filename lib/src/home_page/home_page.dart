29a4b66a4656a4c03c11109551b774c97589a9e2
Brandon Largeau <yberion.ins@gmail.com>
2021-01-18 11:31:54 +0100

    Initialisation du projet avec une Home Page et un Drawer, Wrapper, Firebase Auth Service

---
+++ lib/src/home_page/home_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'widgets/drawer/home_drawer.dart';
+import 'widgets/home_main.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      behavior: HitTestBehavior.translucent,
+      onTap: () {
+        final currentFocus = FocusScope.of(context);
+
+        if (!currentFocus.hasPrimaryFocus) {
+          currentFocus.unfocus();
+        }
+      },
+      child: Scaffold(
+        backgroundColor: const Color(0xff2E4053),
+        appBar: AppBar(
+          backgroundColor: const Color(0xff2E4060),
+          title: const Text('Home page'),
+        ),
+        drawer: const HomeDrawer(),
+        body: const HomeMain(),
+      ),
+    );
+  }
+}
---
+++ lib/src/home_page/widgets/home_body.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomeBody extends StatelessWidget {
+  const HomeBody({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
